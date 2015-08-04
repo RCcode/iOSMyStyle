51b457baaa636b9329c4cac4a219b77faebd8d48
TCH <tch@TCHdeMacBook-Air.local>
2015-08-04 16:07:29 +0800

    首页ui

--- iOSCostumeMatching/View/CollectionView/CHTCollectionViewWaterfallCell.m
+++ iOSCostumeMatching/View/CollectionView/CHTCollectionViewWaterfallCell.m
@@ -33,16 +33,18 @@
 
 - (id)initWithFrame:(CGRect)frame {
   if (self = [super initWithFrame:frame]) {
-    _imageView = [[UIImageView alloc] init];
-    // Scale with fill for contents when we resize.
-    _imageView.contentMode = UIViewContentModeScaleAspectFill;
+      _imageView = [[UIImageView alloc] init];
+      // Scale with fill for contents when we resize.
+      _imageView.contentMode = UIViewContentModeScaleAspectFill;
 
-    // Scale the imageview to fit inside the contentView with the image centered:
-    CGRect imageViewFrame = CGRectMake(0.f, 0.f, CGRectGetMaxX(self.contentView.bounds), CGRectGetMaxY(self.contentView.bounds));
-    _imageView.frame = imageViewFrame;
-    _imageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    _imageView.clipsToBounds = YES;
-    [self.contentView addSubview:_imageView];
+      // Scale the imageview to fit inside the contentView with the image centered:
+      CGRect imageViewFrame = CGRectMake(0.f, 0.f, CGRectGetMaxX(self.contentView.bounds), CGRectGetMaxY(self.contentView.bounds));
+      _imageView.frame = imageViewFrame;
+      _imageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+      _imageView.clipsToBounds = YES;
+      [self.contentView addSubview:_imageView];
+      self.layer.borderWidth = 0.5;
+      self.layer.borderColor = colorWithHexString(@"#e6e6e6").CGColor;
   }
   return self;
 }
